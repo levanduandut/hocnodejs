78ebf3d24395e7ff6f5b4f4a8ea1fa028214b9e3
levanduandut <fbsinon300101@gmail.com>
2022-12-30 22:11:37 +0700

    Ok

--- src/index.js
+++ src/index.js
@@ -11,13 +11,19 @@ const port = 3000
 app.use(morgan('combined'))
 
 //Template enginer
-app.engine('handlebars', handlebars.engine());
-app.set('view engine', 'handlebars');
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
 app.set('views',path.join(__dirname,'resources/views'));
 
 
 app.get('/', (req, res) => {
   res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
 })
 
 app.listen(port, () => {
