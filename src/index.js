a5f3c922dd5ccd709e565b2cc0669d028b0e8874
levanduandut <fbsinon300101@gmail.com>
2023-01-06 17:08:00 +0700

    Load lai

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const path = require("path")
 
 const app = express();
 const port = 3000
-app.use(express.static(path.join(__dirname,'/resources/public')));
+app.use(express.static(path.join(__dirname,'/public')));
 //Http logger
 app.use(morgan('combined'))
 
